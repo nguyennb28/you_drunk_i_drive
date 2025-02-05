ef637db06ab8671372152afb5175c1d7d297f016
nguyennb28 <nguyennb.coding@gmail.com>
2025-02-06 00:04:11 +0700

    add phone field

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -5,7 +5,7 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         # fields = '__all__'
-        fields = ["id", "username", "password"]
+        fields = ["id", "username", "phone", "password"]
         extra_kwargs = {"password": {"write_only": True}}
 
     # def create(self, validated_data):
