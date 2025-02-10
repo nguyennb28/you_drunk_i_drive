955b225839c536db76751ff3f9db53550f6055d2
nguyennb28 <nguyennb.coding@gmail.com>
2025-02-10 16:57:34 +0700

    make validation for creation

--- react-admin-demo-v1/src/pages/users/user-create.tsx
+++ react-admin-demo-v1/src/pages/users/user-create.tsx
@@ -7,24 +7,45 @@ import {
   SelectInput,
   required,
 } from "react-admin";
+import userResolver from "./user-schema";
+
+// const UserCreate = () => (
+//   <Create redirect="list">
+//     <SimpleForm warnWhenUnsavedChanges>
+//       <TextInput source="username" required/>
+//       <TextInput source="phone" required/>
+//       <PasswordInput source="password" required/>
+//       <SelectInput
+//         source="role"
+//         choices={[
+//             { id: "customer", name: "customer" },
+//             { id: "driver", name: "driver" },
+//             { id: "admin", name: "admin" },
+//         ]}
+//         validate={required()} 
+//       />
+//     </SimpleForm>
+//   </Create>
+// );
 
 const UserCreate = () => (
-  <Create redirect="list">
-    <SimpleForm warnWhenUnsavedChanges>
-      <TextInput source="username" required/>
-      <TextInput source="phone" required/>
-      <PasswordInput source="password" required/>
-      <SelectInput
-        source="role"
-        choices={[
-            { id: "customer", name: "customer" },
-            { id: "driver", name: "driver" },
-            { id: "admin", name: "admin" },
-        ]}
-        validate={required()} 
-      />
-    </SimpleForm>
-  </Create>
-);
+    <Create redirect="list">
+      <SimpleForm warnWhenUnsavedChanges
+      resolver={userResolver}>
+        <TextInput source="username" required/>
+        <TextInput source="phone" required/>
+        <PasswordInput source="password" required/>
+        <SelectInput
+          source="role"
+          choices={[
+              { id: "customer", name: "customer" },
+              { id: "driver", name: "driver" },
+              { id: "admin", name: "admin" },
+          ]}
+          validate={required()} 
+        />
+      </SimpleForm>
+    </Create>
+  );
 
 export default UserCreate;
