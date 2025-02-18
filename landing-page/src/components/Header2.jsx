01bcf63c27a0e6d5aa1c39b9d41ec717479f79b3
nguyennb28 <nguyennb.coding@gmail.com>
2025-02-18 10:08:44 +0700

    setup headers

--- landing-page/src/components/Header2.jsx
+++ landing-page/src/components/Header2.jsx
@@ -26,55 +26,32 @@ import {
   ChevronDownIcon,
   PhoneIcon,
   PlayCircleIcon,
+  DocumentCheckIcon,
 } from "@heroicons/react/20/solid";
 
-// const products = [
-//   {
-//     name: "Analytics",
-//     description: "Get a better understanding of your traffic",
-//     href: "#",
-//     icon: ChartPieIcon,
-//   },
-//   {
-//     name: "Engagement",
-//     description: "Speak directly to your customers",
-//     href: "#",
-//     icon: CursorArrowRaysIcon,
-//   },
-//   {
-//     name: "Security",
-//     description: "Your customers’ data will be safe and secure",
-//     href: "#",
-//     icon: FingerPrintIcon,
-//   },
-//   {
-//     name: "Integrations",
-//     description: "Connect with third-party tools",
-//     href: "#",
-//     icon: SquaresPlusIcon,
-//   },
-//   {
-//     name: "Automations",
-//     description: "Build strategic funnels that will convert",
-//     href: "#",
-//     icon: ArrowPathIcon,
-//   },
-// ];
-
 const products = [
-    {
-        name: "Tài xế ô tô",
-        description: "Thuê tài xế ô tô",
-        href: "#",
-        icon: TruckIcon,
-    },
-    {
-        name: "Tài xế đi tỉnh",
-        description: "Thuê tài xế đi tỉnh",
-        href: "#",
-        icon: MapIcon,
-    }
-]
+  {
+    name: "Tài xế ô tô",
+    description: "Thuê tài xế ô tô",
+    href: "#",
+    icon: TruckIcon,
+  },
+  {
+    name: "Tài xế đi tỉnh",
+    description: "Thuê tài xế đi tỉnh",
+    href: "#",
+    icon: MapIcon,
+  },
+];
+
+const subProducts = [
+  {
+    name: "Đăng kiểm hộ",
+    description: "Dịch vụ đăng kiểm hộ",
+    href: "#",
+    icon: DocumentCheckIcon,
+  },
+];
 
 const callsToAction = [
   { name: "Watch demo", href: "#", icon: PlayCircleIcon },
@@ -164,6 +141,49 @@ export default function Header2() {
                   </a>
                 ))}
               </div> */}
+            </PopoverPanel>
+          </Popover>
+
+          {/*  */}
+
+          <Popover className="relative">
+            <PopoverButton className="flex items-center gap-x-1 text-sm/6 font-semibold text-gray-900">
+              Dịch vụ liên kết
+              <ChevronDownIcon
+                aria-hidden="true"
+                className="size-5 flex-none text-gray-400"
+              />
+            </PopoverButton>
+
+            <PopoverPanel
+              transition
+              className="absolute top-full -left-8 z-10 mt-3 w-screen max-w-md overflow-hidden rounded-3xl bg-white ring-1 shadow-lg ring-gray-900/5 transition data-closed:translate-y-1 data-closed:opacity-0 data-enter:duration-200 data-enter:ease-out data-leave:duration-150 data-leave:ease-in"
+            >
+              <div className="p-4">
+                {subProducts.map((item) => (
+                  <div
+                    key={item.name}
+                    className="group relative flex items-center gap-x-6 rounded-lg p-4 text-sm/6 hover:bg-gray-50"
+                  >
+                    <div className="flex size-11 flex-none items-center justify-center rounded-lg bg-gray-50 group-hover:bg-white">
+                      <item.icon
+                        aria-hidden="true"
+                        className="size-6 text-gray-600 group-hover:text-indigo-600"
+                      />
+                    </div>
+                    <div className="flex-auto">
+                      <a
+                        href={item.href}
+                        className="block font-semibold text-gray-900"
+                      >
+                        {item.name}
+                        <span className="absolute inset-0" />
+                      </a>
+                      <p className="mt-1 text-gray-600">{item.description}</p>
+                    </div>
+                  </div>
+                ))}
+              </div>
             </PopoverPanel>
           </Popover>
 
